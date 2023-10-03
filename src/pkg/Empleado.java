6b696e1ab47f246af78941d1f8fb6cfc4c90e604
usuario <usuario@pc20-015>
2023-10-03 11:11:17 +0200

    commit

---
+++ src/pkg/Empleado.java
@@ -0,0 +1,39 @@
+package pkg;
+
+public class Empleado {
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	
+	float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		float salariobase=0, primas=0, extras=0;
+		final int PRECIO_HE=30;
+		
+		if(tipo==TipoEmpleado.Vendedor)
+			salariobase=2000;
+		else if(tipo==TipoEmpleado.Encargado)
+			salariobase=2500;
+		
+		
+		if(ventasMes>=1500)
+			primas=200;
+		else if(ventasMes>=1000)
+			primas=100;
+		
+		extras=horasExtra*PRECIO_HE;
+		
+		return salariobase+primas+extras;
+	}
+	
+	
+	float calculoNominaNeta(float nominaBruta) {
+		float retencion;
+		if(nominaBruta<2100)
+			retencion=0;
+		else if(nominaBruta>=2100 && nominaBruta<2500)
+			retencion=(float) 0.15;
+		else
+			retencion=(float) 0.18;
+		return nominaBruta * (1-retencion);
+	}
+	
+}
